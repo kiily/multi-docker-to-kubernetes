226a3bbb1ce44cdf63f1ebf85974733d4eb4de59
kiily <miguelmarin95@hotmail.com>
2020-11-01 17:24:44 +0000

    add deployment script

--- README.md
+++ README.md
@@ -5,3 +5,5 @@ More repos from the Udemy k8s course by Stephen Grinder
 `master` has the code with kubernetes. The `master-original` branch has the original code.
 
 This will be deployed to a k8s cluster in GCP, using TravisCI.
+
+For the k8s deployment, added a secret for PG_PASSWORD in the remote cluster
---
+++ deploy.sh
@@ -0,0 +1,17 @@
+docker build -t kiily/multi-client-k8s:latest -t kiily/multi-client-k8s:$GIT_SHA -f ./client/Dockerfile ./client
+docker build -t kiily/multi-server-k8s:latest -t kiily/multi-server-k8s:$GIT_SHA -f ./server/Dockerfile ./server
+docker build -t kiily/multi-worker-k8s:latest -t kiily/multi-worker-k8s:$GIT_SHA -f ./worker/Dockerfile ./worker
+
+docker push killy/multi-client-k8s:latest
+docker push killy/multi-server-k8s:latest
+docker push killy/multi-worker-k8s:latest
+
+docker push killy/multi-client-k8s:$GIT_SHA
+docker push killy/multi-server-k8s:$GIT_SHA
+docker push killy/multi-worker-k8s:$GIT_SHA
+
+# Can use kubectl as this runs in travis
+kubectl apply -f k8s
+kubectl set image deployments/client-deployment client=kiily/multi-client-k8s:$GIT_SHA
+kubectl set image deployments/server-deployment server=kiily/multi-server-k8s:$GIT_SHA
+kubectl set image deployments/worker-deployment worker=kiily/multi-worker-k8s:$GIT_SHA
