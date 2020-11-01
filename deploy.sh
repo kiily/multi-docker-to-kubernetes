4eae57d0cc22cda38c75260105c2441c640db44e
kiily <miguelmarin95@hotmail.com>
2020-11-01 17:45:12 +0000

    more fixes to deployment script

--- deploy.sh
+++ deploy.sh
@@ -2,13 +2,13 @@ docker build -t kiily/multi-client-k8s:latest -t kiily/multi-client-k8s:$GIT_SHA
 docker build -t kiily/multi-server-k8s:latest -t kiily/multi-server-k8s:$GIT_SHA -f ./server/Dockerfile ./server
 docker build -t kiily/multi-worker-k8s:latest -t kiily/multi-worker-k8s:$GIT_SHA -f ./worker/Dockerfile ./worker
 
-docker push killy/multi-client-k8s:latest
-docker push killy/multi-server-k8s:latest
-docker push killy/multi-worker-k8s:latest
+docker push kiily/multi-client-k8s:latest
+docker push kiily/multi-server-k8s:latest
+docker push kiily/multi-worker-k8s:latest
 
-docker push killy/multi-client-k8s:$GIT_SHA
-docker push killy/multi-server-k8s:$GIT_SHA
-docker push killy/multi-worker-k8s:$GIT_SHA
+docker push kiily/multi-client-k8s:$GIT_SHA
+docker push kiily/multi-server-k8s:$GIT_SHA
+docker push kiily/multi-worker-k8s:$GIT_SHA
 
 # Can use kubectl as this runs in travis
 kubectl apply -f k8s
